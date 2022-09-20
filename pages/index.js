7859358e052f31f1c7aa156878e0af645c070e3f
Tim Tse <timtse76@gmail.com>
2022-09-20 17:13:39 -0400

    added linkedin

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import CenterTextWrapper from 'components/style/CenterTextWrapper';
+import OutLink from 'components/elements/OutLink';
 
 export default function Home() {
   return (
@@ -16,6 +17,15 @@ export default function Home() {
         <Row>
           <Col>
             <p style={{ textAlign: 'center', fontSize: '1.5rem' }}>Software Engineer based in Northern New Jersey</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div style={{ textAlign: 'center' }}>
+            <span>
+              <OutLink href="https://github.com/ttse76">Github</OutLink>&nbsp;&nbsp;&nbsp;&nbsp;<OutLink href="https://www.linkedin.com/in/timothy-tse-723269103/">LinkedIn</OutLink>
+            </span>
+            </div>
           </Col>
         </Row>
       </CenterTextWrapper>
