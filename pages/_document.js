69c5f219a9a2bb5f235715ff805ee7c91ab5b561
Tim Tse <timtse76@gmail.com>
2022-09-20 17:30:24 -0400

    added icon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/icon.png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
         <link href={'https://fonts.googleapis.com/css2?family=Play&family=Share+Tech+Mono&display=swap'} rel="stylesheet" />
