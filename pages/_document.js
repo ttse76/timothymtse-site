818a9feb098e1846cb2d2aaceebab9bdbe0a93f1
Tim Tse <timtse76@gmail.com>
2022-09-20 16:20:02 -0400

    init

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <Main />
+      <NextScript />
+    </Html>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function Home() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Timothy M. Tse</h1>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
