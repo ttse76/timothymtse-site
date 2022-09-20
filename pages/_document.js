f23e1ebd1a95d58d3af54f8f4c2d127608779289
Tim Tse <timtse76@gmail.com>
2022-09-20 18:21:07 -0400

    changed cross origin to string

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href={'https://fonts.googleapis.com/css2?family=Play&family=Share+Tech+Mono&display=swap'} rel="stylesheet" />
       </Head>
       <Main />
