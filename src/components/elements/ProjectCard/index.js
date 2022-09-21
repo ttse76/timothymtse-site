2147fc7364bee1d2d509fc8cb3bc5bd0899bf38b
Tim Tse <timtse76@gmail.com>
2022-09-20 21:47:39 -0400

    added styling

--- src/components/elements/ProjectCard/index.js
+++ src/components/elements/ProjectCard/index.js
@@ -1,10 +1,12 @@
 import Card from 'react-bootstrap/Card';
 
+import styles from 'styles/components/elements/ProjectCard.module.css';
+
 export default function ProjectCard({ title, description, links }) {
   return (
-    <Card>
+    <Card className={styles.card}>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className={styles.cardTitle}>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
       </Card.Body>
     </Card>
