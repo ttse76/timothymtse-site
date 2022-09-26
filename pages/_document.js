a5fbe20eab2e62e2153193b6cc9692300083d6bd
Tim Tse <timtse76@gmail.com>
2022-09-25 22:32:45 -0400

    added meta data in head

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href={'https://fonts.googleapis.com/css2?family=Play&family=Share+Tech+Mono&display=swap'} rel="stylesheet" />
+        <meta author="Timothy Tse" />
+        <mets description="Personal webstie for senior software engineer Timothy Tse" />
       </Head>
       <Main />
       <NextScript />
