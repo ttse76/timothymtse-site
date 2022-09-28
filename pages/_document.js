9c9dc8f58d9ae24e6292c6e3a3a9566fe2f19b61
Tim Tse <timtse76@gmail.com>
2022-09-28 16:24:56 -0400

    changed name

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href={'https://fonts.googleapis.com/css2?family=Play&family=Share+Tech+Mono&display=swap'} rel="stylesheet" />
         <meta author="Timothy Tse" />
-        <mets description="Personal webstie for senior software engineer Timothy Tse based in the New Jersey/New York area" />
+        <meta description="Personal webstie for senior software engineer Timothy Tse based in the New Jersey/New York area" />
       </Head>
       <Main />
       <NextScript />
--- src/components/document/PageWrapper/index.js
+++ src/components/document/PageWrapper/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
-export default function PageWrapper({ children, title = "Timothy M. Tse", backButton = false }) {
+export default function PageWrapper({ children, title = "Timothy Tse", backButton = false }) {
   
   const back = backButton ? <Nav>
     <Nav.Link href="/"><span style={{fontSize: '2rem'}} className="tim-link">&larr; back</span></Nav.Link>
