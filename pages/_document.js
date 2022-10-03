247c6244e1c36a7d5091ff138663a6769ccbde23
Tim Tse <timtse76@gmail.com>
2022-10-02 20:20:27 -0400

    added config palette

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
+import { config } from 'config';
+
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const palettes = {
+    1: 'https://fonts.googleapis.com/css2?family=Play&family=Share+Tech+Mono&display=swap'
+  }
   return (
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href={'https://fonts.googleapis.com/css2?family=Play&family=Share+Tech+Mono&display=swap'} rel="stylesheet" />
+        <link href={palettes[config['palette']]} rel="stylesheet" />
         <meta author="Timothy Tse" />
         <meta description="Personal webstie for senior software engineer Timothy Tse based in the New Jersey/New York area" />
       </Head>
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+export const config = {
+  palette: 1
+}
