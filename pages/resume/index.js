8ecb544e57cd2999eba6ffb432ac13d4dd97afd3
Tim Tse <timtse76@gmail.com>
2023-01-01 14:00:09 -0500

    added resume info

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -5,22 +7,37 @@ import PageWrapper from "components/document/PageWrapper";
 import ResumeItem from 'components/elements/ResumeItem';
 import Accordion from 'react-bootstrap/Accordion';
 
+import styles from 'styles/pages/Resume.module.css';
+
 export default function Resume({ resume, skills }) {
   const items = resume.map((resumeItem, index) => {
     return <ResumeItem {...resumeItem} eventKey={index} key={index} />
   });
+
+  const paragraphStyle = {
+    textAlign: 'justify'
+  }
 
   return (
     <PageWrapper title="Resume" backButton={true}>
       <Row className="pt-5 justify-content-md-center">
         <Col lg={8}>
           <h1 className="section-header pb-2">Hello, World!</h1>
-          <p style={{textAlign: 'justify'}}>
+          <p className={styles.paragraph}>
             My name is Timothy Tse. I am a software engineer based in northern New Jersey,
             just outside of New York City. I have over 5 years of experience
             working with companies accross many different industries, working on teams both big
             and small, with extensive experience and knowlege in both frontend and backend systems,
             working with the most popular technologies on the market.
+          </p>
+          <br />
+          <p className={styles.paragraph}>
+            In addition to programming, I have been heavily involved in software design and architecture,
+            working with both engineers and managers to design and develop major features for enterprise applications.
+          </p>
+          <br />
+          <p style={paragraphStyle}>
+            I am also open to select freelance web development projects as well. For inquries, feel free to email me <Link href="/email"><a className={styles.paragraphLink}>here!</a></Link>
           </p>
         </Col>
       </Row>
